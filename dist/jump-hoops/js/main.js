360de40936082ece287c2781a182dbbbb0793373
Noah Cooper <noah.cooper@hotmail.com>
2017-02-13 00:53:45 -0500

    Add Jump/Hoops JS

---
+++ dist/jump-hoops/js/main.js
@@ -0,0 +1,38 @@
+(function() {
+  angular.module('ahaLuminateApp', ['ngSanitize', 'ui.bootstrap', 'ahaLuminateControllers']);
+
+  angular.module('ahaLuminateControllers', []);
+
+  angular.module('ahaLuminateApp').constant('APP_INFO', {
+    version: '1.0.0'
+  });
+
+  angular.module('ahaLuminateApp').run([
+    '$rootScope', 'APP_INFO', function($rootScope, APP_INFO) {
+      var $dataRoot;
+      $dataRoot = angular.element('[data-aha-luminate-root]');
+      if ($dataRoot.data('api-key') !== '') {
+        $rootScope.apiKey = $dataRoot.data('api-key');
+      }
+      if (!$rootScope.apiKey) {
+        new Error('AHA Luminate Framework: No Luminate Online API Key is defined.');
+      }
+      if ($dataRoot.data('cons-id') !== '') {
+        $rootScope.consId = $dataRoot.data('cons-id');
+      }
+      if ($dataRoot.data('auth-token') !== '') {
+        $rootScope.authToken = $dataRoot.data('auth-token');
+      }
+      if ($dataRoot.data('fr-id') !== '') {
+        return $rootScope.frId = $dataRoot.data('fr-id');
+      }
+    }
+  ]);
+
+  angular.element(document).ready(function() {
+    var appModules;
+    appModules = ['ahaLuminateApp'];
+    return angular.bootstrap(document, appModules);
+  });
+
+}).call(this);
---
+++ src/jump-hoops/coffee/init.coffee
@@ -0,0 +1,33 @@
+angular.module 'ahaLuminateApp', [
+  'ngSanitize'
+  'ui.bootstrap'
+  'ahaLuminateControllers'
+]
+
+angular.module 'ahaLuminateControllers', []
+
+angular.module 'ahaLuminateApp'
+  .constant 'APP_INFO', 
+    version: '1.0.0'
+
+angular.module 'ahaLuminateApp'
+  .run [
+    '$rootScope'
+    'APP_INFO'
+    ($rootScope, APP_INFO) ->
+      # get data from root element
+      $dataRoot = angular.element '[data-aha-luminate-root]'
+      $rootScope.apiKey = $dataRoot.data('api-key') if $dataRoot.data('api-key') isnt ''
+      if not $rootScope.apiKey
+        new Error 'AHA Luminate Framework: No Luminate Online API Key is defined.'
+      $rootScope.consId = $dataRoot.data('cons-id') if $dataRoot.data('cons-id') isnt ''
+      $rootScope.authToken = $dataRoot.data('auth-token') if $dataRoot.data('auth-token') isnt ''
+      $rootScope.frId = $dataRoot.data('fr-id') if $dataRoot.data('fr-id') isnt ''
+  ]
+
+angular.element(document).ready ->
+  appModules = [
+    'ahaLuminateApp'
+  ]
+  
+  angular.bootstrap document, appModules
