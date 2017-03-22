039c259ad904d25a2c4ed8d38452698d19fbf752
Noah Cooper <noah.cooper@hotmail.com>
2017-03-22 18:36:25 -0400

    Add injector task for general CSS and JS

--- grunt/task/injector.js
+++ grunt/task/injector.js
@@ -5,9 +5,44 @@ module.exports = {
     min: true
   }, 
   
+  "general-css-main": {
+    options: {
+      template: 'src/general/html/page-wrapper/head-styles.html', 
+      starttag: '<!-- general-css-injector:{{ext}} -->', 
+      endtag: '<!-- general-css-endinjector -->', 
+      transform: function(filepath) {
+        var timestamp = filepath.split('.')[1]; // Get the timestamp out of the filename
+        return '<link rel="stylesheet" href="../[[?xx::x[[S80:dev_branch]]x::::[[S80:dev_branch]]/]]aha-luminate/dist/general/css/main.' + timestamp + '[[?xtruex::x[[S80:debug]]x::::.min]].css">';
+      }
+    }, 
+    files: {
+      'src/general/html/page-wrapper/head-styles.html': [
+        'dist/general/css/main.' + '<%= timestamp %>' + '.min.css'
+      ]
+    }
+  }, 
+  
+  "general-js-main": {
+    options: {
+      template: 'src/general/html/page-wrapper/body-scripts.html', 
+      starttag: '<!-- general-js-injector:{{ext}} -->', 
+      endtag: '<!-- general-js-endinjector -->', 
+      transform: function(filepath) {
+        var timestamp = filepath.split('.')[1]; // Get the timestamp out of the filename
+        return '<script src="../[[?xx::x[[S80:dev_branch]]x::::[[S80:dev_branch]]/]]aha-luminate/dist/general/js/main.' + timestamp + '[[?xtruex::x[[S80:debug]]x::::.min]].js"></script>';
+      }
+    }, 
+    
+    files: {
+      'src/general/html/page-wrapper/body-scripts.html': [
+        'dist/general/js/main.' + '<%= timestamp %>' + '.min.js'
+      ]
+    }
+  }, 
+  
   "jump-hoops-css-main": {
     options: {
-      template: 'src/jump-hoops/html/page-wrapper/head-styles.html',
+      template: 'src/jump-hoops/html/page-wrapper/head-styles.html', 
       starttag: '<!-- jump-hoops-css-injector:{{ext}} -->', 
       endtag: '<!-- jump-hoops-css-endinjector -->', 
       transform: function(filepath) {
@@ -16,13 +51,15 @@ module.exports = {
       }
     }, 
     files: {
-      'src/jump-hoops/html/page-wrapper/head-styles.html': ['dist/jump-hoops/css/main.' + '<%= timestamp %>' + '.min.css']
+      'src/jump-hoops/html/page-wrapper/head-styles.html': [
+        'dist/jump-hoops/css/main.' + '<%= timestamp %>' + '.min.css'
+      ]
     }
-  },
-
+  }, 
+  
   "jump-hoops-css-participant": {
     options: {
-      template: 'src/jump-hoops/html/page-wrapper/head-styles.html',
+      template: 'src/jump-hoops/html/page-wrapper/head-styles.html', 
       starttag: '<!-- jump-hoops-participant-css-injector:{{ext}} -->', 
       endtag: '<!-- jump-hoops-participant-css-endinjector -->', 
       transform: function(filepath) {
@@ -31,29 +68,33 @@ module.exports = {
       }
     }, 
     files: {
-      'src/jump-hoops/html/page-wrapper/head-styles.html': ['dist/jump-hoops/css/participant.' + '<%= timestamp %>' + '.min.css']
+      'src/jump-hoops/html/page-wrapper/head-styles.html': [
+        'dist/jump-hoops/css/participant.' + '<%= timestamp %>' + '.min.css'
+      ]
     }
-  },  
+  }, 
   
   "jump-hoops-js-main": {
     options: {
-      template: 'src/jump-hoops/html/page-wrapper/body-scripts.html',
+      template: 'src/jump-hoops/html/page-wrapper/body-scripts.html', 
       starttag: '<!-- jump-hoops-js-injector:{{ext}} -->', 
       endtag: '<!-- jump-hoops-js-endinjector -->', 
       transform: function(filepath) {
         var timestamp = filepath.split('.')[1]; // Get the timestamp out of the filename
         return '<script src="../[[?xx::x[[S80:dev_branch]]x::::[[S80:dev_branch]]/]]aha-luminate/dist/jump-hoops/js/main.' + timestamp + '[[?xtruex::x[[S80:debug]]x::::.min]].js"></script>';
       }
-    },
+    }, 
     
     files: {
-      'src/jump-hoops/html/page-wrapper/body-scripts.html': ['dist/jump-hoops/js/main.' + '<%= timestamp %>' + '.min.js']
+      'src/jump-hoops/html/page-wrapper/body-scripts.html': [
+        'dist/jump-hoops/js/main.' + '<%= timestamp %>' + '.min.js'
+      ]
     }
   }, 
   
   "jump-hoops-js-participant": {
     options: {
-      template: 'src/jump-hoops/html/page-wrapper/body-scripts.html',
+      template: 'src/jump-hoops/html/page-wrapper/body-scripts.html', 
       starttag: '<!-- jump-hoops-participant-js-injector:{{ext}} -->', 
       endtag: '<!-- jump-hoops-participant-js-endinjector -->', 
       transform: function(filepath) {
@@ -62,7 +103,9 @@ module.exports = {
       }
     }, 
     files: {
-      'src/jump-hoops/html/page-wrapper/body-scripts.html': ['dist/jump-hoops/js/participant.' + '<%= timestamp %>' + '.min.js']
+      'src/jump-hoops/html/page-wrapper/body-scripts.html': [
+        'dist/jump-hoops/js/participant.' + '<%= timestamp %>' + '.min.js'
+      ]
     }
   }
 }
