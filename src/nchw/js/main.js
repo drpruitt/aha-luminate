260dcbe6a485a8386ea0b7ef310aee73d299bf7f
Colin Manuel <colin.manuel@gmail.com>
2018-03-02 14:21:29 -0600

    add sample js and css

--- src/nchw/js/main.js
+++ src/nchw/js/main.js
@@ -1,6 +1,12 @@
 'use strict';
 (function ($) {
   $(document).ready(function ($) {
-  // New NCHW JS goes here...
+    // New NCHW JS goes here...
+    // Example of retrieving data attribute (fr_id) from the body tag
+    var evID = $('body').data('fr-id') ? $('body').data('fr-id') : null;
+
+    if ($('body').is('.pg_entry')) {
+      // Greeting page-specific JS goes here
+    }
   });
 }(jQuery));
