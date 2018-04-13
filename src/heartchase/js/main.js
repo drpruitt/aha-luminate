d9bb8c8cd5b19296fa0796b90ce99ff487be77ea
ironman67x <dean@dhwebworks.com>
2018-04-13 08:38:47 -0700

    change nchw to heartchase

--- src/heartchase/js/main.js
+++ src/heartchase/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 (function ($) {
   $(document).ready(function ($) {
-    // New NCHW JS goes here...
+    // New HeartChase JS goes here...
     // Example of retrieving data attribute (fr_id) from the body tag
     var evID = $('body').data('fr-id') ? $('body').data('fr-id') : null;
     console.log("Event ID = ",evID);
