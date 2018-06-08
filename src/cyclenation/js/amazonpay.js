a3a4b8f70babc323885b6d974f41e17417f7d886
ironman67x <dean@dhwebworks.com>
2018-06-07 17:56:16 -0700

    add getCustomQuerystring

--- src/cyclenation/js/amazonpay.js
+++ src/cyclenation/js/amazonpay.js
@@ -1,3 +1,18 @@
+(function ($) {
+	$.extend({
+		getCustomQuerystring: function(url, name){
+		  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+		  var regexS = "[\\?&]" + name + "=([^&#]*)";
+		  var regex = new RegExp(regexS);
+		  var results = regex.exec(url);
+		  if(results == null)
+			return "";
+		  else
+			return decodeURIComponent(results[1].replace(/\+/g, " "));
+		}
+	});
+})(jQuery);
+
 /* amazon pay code here */
 var default_path = "https://www2.heart.org";
 jQuery(document).ready(function(){
