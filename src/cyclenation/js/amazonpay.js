bb39cc58e3cffb84538bed0066fb1b9ed451d51d
ironman67x <dean@dhwebworks.com>
2018-06-13 11:32:42 -0700

    make donate button toggle payment types

--- src/cyclenation/js/amazonpay.js
+++ src/cyclenation/js/amazonpay.js
@@ -104,14 +104,19 @@ function addPCPaymentOptions() {
 function addPaymentTypesOnSearch() {
 	jQuery('a:contains(Donate)').on('click',function(e){
 		e.preventDefault();
-		var dlink = jQuery(this).attr("href");
-		var fr_id = jQuery.getCustomQuerystring(dlink,"FR_ID");
-		var px = jQuery.getCustomQuerystring(dlink,"PROXY_ID");
-		var pt = jQuery.getCustomQuerystring(dlink,"PROXY_TYPE");
-		var html = "<div class='paymentSelType text-center'><h6>How would you like to donate?</h6>" +
-  			"<a href='"+dlink+"'><img src='https://www2.heart.org/images/content/pagebuilder/credit-card-logos2.png'/></a>" +
-  			"<a href='https://secure3.convio.net/heartdev/site/SPageNavigator/cyclenation_donate_amazon.html?FR_ID="+fr_id+"&mfc_pref=T&PROXY_ID="+px+"&PROXY_TYPE="+pt+"' class='amazon'><img src='https://donatenow.heart.org/images/amazon-payments_inactive.png'/></a>" +
-  			"<a href='"+dlink+"&paypal=true'><img src='https://www2.heart.org/images/content/pagebuilder/PP_logo_h_100x26.png'/></a>";
-		jQuery(this).after(html);
+		if (jQuery(this).next('.paymentSelType').length > 0) {
+			jQuery(this).next('.paymentSelType').remove();
+		} else {
+			var dlink = jQuery(this).attr("href");
+			var fr_id = jQuery.getCustomQuerystring(dlink,"FR_ID");
+			var px = jQuery.getCustomQuerystring(dlink,"PROXY_ID");
+			var pt = jQuery.getCustomQuerystring(dlink,"PROXY_TYPE");
+			var html = "<div class='paymentSelType text-center'>" +
+				"<h6>How would you like to donate?</h6>" +
+				"<a href='"+dlink+"'><img src='https://www2.heart.org/images/content/pagebuilder/credit-card-logos2.png'/></a>" +
+				"<a href='https://secure3.convio.net/heartdev/site/SPageNavigator/cyclenation_donate_amazon.html?FR_ID="+fr_id+"&mfc_pref=T&PROXY_ID="+px+"&PROXY_TYPE="+pt+"' class='amazon'><img src='https://donatenow.heart.org/images/amazon-payments_inactive.png'/></a>" +
+				"<a href='"+dlink+"&paypal=true'><img src='https://www2.heart.org/images/content/pagebuilder/PP_logo_h_100x26.png'/></a>";
+			jQuery(this).after(html);
+		}
 	});
 }
